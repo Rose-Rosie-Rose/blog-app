8f88f3c7eb6b32d33db254f1ff92bd3c28be2186
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-04-19 21:33:39 +0900

    design: main page css 작성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export const HomePage = () => {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <header>
+        <div>
+          <Link to="posts/new">글쓰기</Link>
+          <Link to="posts">게시글</Link>
+          <Link to="profile">프로필</Link>
+        </div>
+      </header>
+      <div className="post-list">post List</div>
+      <footer>
+        <div>Menu 1</div>
+        <div>Menu 2</div>
+        <div>Menu 3</div>
+      </footer>
+    </>
+  );
 };
