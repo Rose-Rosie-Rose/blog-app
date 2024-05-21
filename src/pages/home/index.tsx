6d2d4eff83d52d136c85ca8c868d49198c960285
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-05-21 21:14:27 +0900

    docs: 메인 페이지 기본 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -13,7 +13,19 @@ export const HomePage = () => {
       <div className="post__list">
         {[...Array(10)].map((e, index) => (
           <div key={index} className="post__box">
-            <Link to={`/posts/${index}`}>게시글 {index}</Link>
+            <Link to={`/posts/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">작성자이름</div>
+                <div className="post__date">2024.05.21 화요일</div>
+              </div>
+              <div className="post__title">게시글 {index}</div>
+              <div className="post__text">게시글 내용</div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
