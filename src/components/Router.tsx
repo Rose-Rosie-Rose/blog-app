8cdc07fb0eaa74ab14242114f928b65988e616b0
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-10-25 17:18:18 +0900

    docs: 로그인 페이지 기본 설정 루트 폴더 수정

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import {
   HomePage,
@@ -12,18 +12,31 @@ import {
 } from "pages";
 
 export const Router = () => {
+  // firebase Auth가 인증되었으면 true로 변경해주는 로직 추가
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/posts" element={<PostListPage />} />
-        <Route path="/posts/:id" element={<DetailPage />} />
-        <Route path="/posts/new" element={<PostNewPage />} />
-        <Route path="/posts/edit/:ud" element={<PostEditPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/posts" element={<PostListPage />} />
+            <Route path="/posts/:id" element={<DetailPage />} />
+            <Route path="/posts/new" element={<PostNewPage />} />
+            <Route path="/posts/edit/:ud" element={<PostEditPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/*" element={<LoginPage />} />
+          </>
+        )}
       </Routes>
     </>
   );
