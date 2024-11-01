f00bd1c31e210efb49f8fde79514096aafad9caa
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-11-01 15:24:32 +0900

    docs: 로그인 인증 관련 페이지 전환 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Router } from "components";
 
 const App = () => {
   const auth = getAuth(app);
-  const [isAuthenticated, setisAuthenticated] = useState<boolean>(
+
+  // auth를 체크하기 전에(initialize 전) loader를 띄워주는 용도
+  const [init, setInit] = useState<boolean>(false);
+  // auth의 currentUser가 있으면 authenticated로 변경
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
 
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : "init"}
     </>
   );
 };
