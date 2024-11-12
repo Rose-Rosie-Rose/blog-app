96efc5cae8f5dfc062c1ad827470cf667f014b38
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-11-12 15:45:56 +0900

    docs: footer 부분 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <footer>
-      <Link to=" ">Menu 1</Link>
-      <Link to="">Menu 2</Link>
-      <Link to="">Menu 3</Link>
+      <Link to="/posts/new">글쓰기</Link>
+      <Link to="/posts">게시글</Link>
+      <Link to="/profile">프로필</Link>
     </footer>
   );
 };
