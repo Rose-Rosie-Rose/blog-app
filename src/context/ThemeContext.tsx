c43e98483dea3bc94eabc4565aa90d6b70169bc0
Rosie-Rose-Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-11-12 16:02:58 +0900

    docs: react-icons 설치 및 footer에 ThemeContext 적용

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
 
-const ThemeContext = createContext({
+export const ThemeContext = createContext({
   theme: "light",
   toggleMode: () => {},
 });
